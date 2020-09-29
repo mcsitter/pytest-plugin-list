9d9ec66dec77c2e3d0d8be9f1b60138caea111b7
Maximilian Cosmo Sitter <48606431+mcsitter@users.noreply.github.com>
2020-09-29 11:53:42 +0000

    Add plugin_list.py

---
+++ plugin_list.py
@@ -0,0 +1,44 @@
+import re
+
+import requests
+import tabulate
+
+FILE_HEAD = R"""Plugins List
+============
+
+The plugins are listed automatically from PyPI.
+Only PyPI projects that match "pytest-\*" are considered plugins.
+Packages classified as inactive are also excluded.
+
+"""
+
+
+def iter_plugins():
+    regex = r">([\d\w-]*)</a>"
+    response = requests.get("https://pypi.org/simple")
+    for match in re.finditer(regex, response.text):
+        name = match.groups()[0]
+        if not name.startswith("pytest-"):
+            continue
+        response = requests.get(f"https://pypi.org/pypi/{name}/json")
+        if not response.ok:
+            continue
+        info = response.json()["info"]
+        if "Development Status :: 7 - Inactive" in info["classifiers"]:
+            continue
+        yield {
+            "name": f'`{info["name"]} <{info["project_url"]}>`_',
+            "summary": info["summary"],
+        }
+
+
+def main():
+    plugin_table = tabulate.tabulate(iter_plugins(), headers="keys", tablefmt="rst")
+    with open("plugin_list.rst", "w") as f:
+        f.write(FILE_HEAD)
+        f.write(plugin_table)
+        f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
